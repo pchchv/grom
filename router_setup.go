f4aa8402360526475b7e9c742637e54d679deae7
pchchv <ipchchv@gmail.com>
2024-01-20 21:00:30 +0300

    Add a NextMiddlewareFunc type

--- router_setup.go
+++ router_setup.go
@@ -11,3 +11,8 @@ const (
 )
 
 type httpMethod string
+
+// NextMiddlewareFunc are functions passed into your middleware. To advance the middleware, call the function.
+// You should usually pass the existing ResponseWriter and *Request into the next middlware, but you can
+// chose to swap them if you want to modify values or capture things written to the ResponseWriter.
+type NextMiddlewareFunc func(ResponseWriter, *Request)
