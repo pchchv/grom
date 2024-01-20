41acd3309f01e20ef8c2ffc335219594db6f5ef5
pchchv <ipchchv@gmail.com>
2024-01-20 13:18:22 +0300

    Add a http methods consts

--- router_setup.go
+++ router_setup.go
@@ -1,3 +1,13 @@
 package grom
 
+const (
+	httpMethodGet     = httpMethod("GET")
+	httpMethodPost    = httpMethod("POST")
+	httpMethodPut     = httpMethod("PUT")
+	httpMethodDelete  = httpMethod("DELETE")
+	httpMethodPatch   = httpMethod("PATCH")
+	httpMethodHead    = httpMethod("HEAD")
+	httpMethodOptions = httpMethod("OPTIONS")
+)
+
 type httpMethod string
