802c069dc80f72e5933d7908f69b8987be13eae8
pchchv <ipchchv@gmail.com>
2024-01-24 10:48:30 +0300

    Add a logPanicReporter type

--- panic_handler.go
+++ panic_handler.go
@@ -1,9 +1,15 @@
 package grom
 
+import "log"
+
 // PanicReporter can receive panics that happen when serving
 // a request and report them to a log of some sort.
 type PanicReporter interface {
 	// Panic is called with the URL of the request,
 	// the result of calling recover, and the stack.
 	Panic(url string, err interface{}, stack string)
+}
+
+type logPanicReporter struct {
+	log *log.Logger
 }
