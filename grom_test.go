9eaa283a1458deb168993623836661981fd1a621
pchchv <ipchchv@gmail.com>
2024-01-24 11:29:30 +0300

    Implement an init and callerInfo functions

--- grom_test.go
+++ grom_test.go
@@ -1,7 +1,31 @@
 package grom
 
+import (
+	"fmt"
+	"runtime"
+	"strings"
+)
+
 type nullPanicReporter struct{}
 
 func (l nullPanicReporter) Panic(url string, err interface{}, stack string) {
 	// no op
+}
+
+func init() {
+	// This disables printing panics to stderr during testing, because that is very noisy,
+	// and we purposefully test some panics.
+	PanicHandler = nullPanicReporter{}
+}
+
+// callerInfo returns the caller's caller info.
+func callerInfo() string {
+	_, file, line, ok := runtime.Caller(2)
+	if !ok {
+		return ""
+	}
+
+	parts := strings.Split(file, "/")
+	file = parts[len(parts)-1]
+	return fmt.Sprintf("%s:%d", file, line)
 }
