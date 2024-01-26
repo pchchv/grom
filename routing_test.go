04f66f51369e0075a3fba5daf9eacd828a154d16
pchchv <ipchchv@gmail.com>
2024-01-26 13:12:04 +0300

    Implement a routes tests

--- routing_test.go
+++ routing_test.go
@@ -2,7 +2,12 @@ package grom
 
 import (
 	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"sort"
+	"strings"
+	"testing"
 )
 
 type Ctx struct{}
@@ -41,4 +46,136 @@ func stringifyMap(m map[string]string) string {
 	b.WriteRune(']')
 
 	return b.String()
+}
+
+func TestRoutes(t *testing.T) {
+	router := New(Ctx{})
+	table := []routeTest{
+		{
+			route: "/",
+			get:   "/",
+			vars:  nil,
+		},
+		{
+			route: "/api/action",
+			get:   "/api/action",
+			vars:  nil,
+		},
+		{
+			route: "/admin/action",
+			get:   "/admin/action",
+			vars:  nil,
+		},
+		{
+			route: "/admin/action.json",
+			get:   "/admin/action.json",
+			vars:  nil,
+		},
+		{
+			route: "/:api/action",
+			get:   "/poop/action",
+			vars:  map[string]string{"api": "poop"},
+		},
+		{
+			route: "/api/:action",
+			get:   "/api/poop",
+			vars:  map[string]string{"action": "poop"},
+		},
+		{
+			route: "/:seg1/:seg2/bob",
+			get:   "/a/b/bob",
+			vars:  map[string]string{"seg1": "a", "seg2": "b"},
+		},
+		{
+			route: "/:seg1/:seg2/ron",
+			get:   "/c/d/ron",
+			vars:  map[string]string{"seg1": "c", "seg2": "d"},
+		},
+		{
+			route: "/:seg1/:seg2/:seg3",
+			get:   "/c/d/wat",
+			vars:  map[string]string{"seg1": "c", "seg2": "d", "seg3": "wat"},
+		},
+		{
+			route: "/:seg1/:seg2/ron/apple",
+			get:   "/c/d/ron/apple",
+			vars:  map[string]string{"seg1": "c", "seg2": "d"},
+		},
+		{
+			route: "/:seg1/:seg2/ron/:apple",
+			get:   "/c/d/ron/orange",
+			vars:  map[string]string{"seg1": "c", "seg2": "d", "apple": "orange"},
+		},
+		{
+			route: "/site2/:id:\\d+",
+			get:   "/site2/123",
+			vars:  map[string]string{"id": "123"},
+		},
+		{
+			route: "/site2/:id:[a-z]+",
+			get:   "/site2/abc",
+			vars:  map[string]string{"id": "abc"},
+		},
+		{
+			route: "/site2/:id:\\d[a-z]+",
+			get:   "/site2/1abc",
+			vars:  map[string]string{"id": "1abc"},
+		},
+		{
+			route: "/site2/:id",
+			get:   "/site2/1abc1",
+			vars:  map[string]string{"id": "1abc1"},
+		},
+		{
+			route: "/site2/:id:\\d+/other/:var:[A-Z]+",
+			get:   "/site2/123/other/OK",
+			vars:  map[string]string{"id": "123", "var": "OK"},
+		},
+		{
+			route: "/site2/:id/:*",
+			get:   "/site2/1abc1/foo/bar/baz/boo",
+			vars:  map[string]string{"id": "1abc1", "*": "foo/bar/baz/boo"},
+		},
+		{
+			route: "/site3/:id:\\d+/:*",
+			get:   "/site3/123/foo/bar/baz/boo",
+			vars:  map[string]string{"id": "123", "*": "foo/bar/baz/boo"},
+		},
+		{
+			route: "/site3/:*",
+			get:   "/site3/foo/bar/baz/boo",
+			vars:  map[string]string{"*": "foo/bar/baz/boo"},
+		},
+	}
+
+	// Create routes
+	for _, rt := range table {
+		// ensure closure is created per iteraction (it fails otherwise)
+		func(exp string) {
+			router.Get(rt.route, func(w ResponseWriter, r *Request) {
+				w.Header().Set("X-VARS", stringifyMap(r.PathParams))
+				fmt.Fprintf(w, exp)
+			})
+		}(rt.route)
+	}
+
+	// execute them all:
+	for _, rt := range table {
+		recorder := httptest.NewRecorder()
+		request, _ := http.NewRequest("GET", rt.get, nil)
+
+		router.ServeHTTP(recorder, request)
+
+		if recorder.Code != 200 {
+			t.Error("Test:", rt, " Didn't get Code=200. Got Code=", recorder.Code)
+		}
+		body := strings.TrimSpace(string(recorder.Body.Bytes()))
+		if body != rt.route {
+			t.Error("Test:", rt, " Didn't get Body=", rt.route, ". Got Body=", body)
+		}
+		vars := recorder.Header().Get("X-VARS")
+		if vars != stringifyMap(rt.vars) {
+			t.Error("Test:", rt, " Didn't get Vars=", rt.vars, ". Got Vars=", vars)
+		}
+	}
 }
