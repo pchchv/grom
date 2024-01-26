502e37b3c7399caa81af74b629632eefef7762af
pchchv <ipchchv@gmail.com>
2024-01-26 13:10:52 +0300

    Implement a stringifyMap function

--- routing_test.go
+++ routing_test.go
@@ -1,9 +1,44 @@
 package grom
 
+import (
+	"bytes"
+	"sort"
+)
+
 type Ctx struct{}
 
 type routeTest struct {
 	route string
 	get   string
 	vars  map[string]string
+}
+
+// Converts the map into a consistent, string-comparable string (to compare with another map)
+// Eg, stringifyMap({"foo": "bar"}) == stringifyMap({"foo": "bar"})
+func stringifyMap(m map[string]string) string {
+	if m == nil {
+		return ""
+	}
+
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	keysLenMinusOne := len(keys) - 1
+
+	var b bytes.Buffer
+	b.WriteString("[")
+	for i, k := range keys {
+		b.WriteString(k)
+		b.WriteRune(':')
+		b.WriteString(m[k])
+
+		if i != keysLenMinusOne {
+			b.WriteRune(' ')
+		}
+	}
+	b.WriteRune(']')
+
+	return b.String()
 }
