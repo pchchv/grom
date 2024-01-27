bf944d59107ba39d6797552b0d3b13883bab23e9
pchchv <ipchchv@gmail.com>
2024-01-27 12:15:06 +0300

    Implement an ErrorHandlerWithNoContext methods

---
+++ error_test.go
@@ -0,0 +1,11 @@
+package grom
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func ErrorHandlerWithNoContext(w ResponseWriter, r *Request, err interface{}) {
+	w.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprintf(w, "Contextless Error")
+}
