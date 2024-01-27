1f61032f8ab6841607da3a66ca4cf7b0fc23f77c
pchchv <ipchchv@gmail.com>
2024-01-27 12:20:59 +0300

    Implement a contextless error and consistent context tests

--- error_test.go
+++ error_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func ErrorHandlerWithNoContext(w ResponseWriter, r *Request, err interface{}) {
@@ -111,4 +113,34 @@ func TestNonRootMiddlewarePanic(t *testing.T) {
 	rw, req := newTestRequest("GET", "/admin/action")
 	router.ServeHTTP(rw, req)
 	assertResponse(t, rw, "Admin Error", 500)
+}
+
+func TestContextlessError(t *testing.T) {
+	router := New(Context{})
+	router.Error(ErrorHandlerWithNoContext)
+	router.Get("/action", (*Context).ErrorAction)
+
+	admin := router.Subrouter(AdminContext{}, "/admin")
+	admin.Get("/action", (*AdminContext).ErrorAction)
+
+	rw, req := newTestRequest("GET", "/action")
+	router.ServeHTTP(rw, req)
+	assert.Equal(t, http.StatusInternalServerError, rw.Code)
+	assertResponse(t, rw, "Contextless Error", http.StatusInternalServerError)
+
+	rw, req = newTestRequest("GET", "/admin/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "Contextless Error", http.StatusInternalServerError)
+}
+
+func TestConsistentContext(t *testing.T) {
+	router := New(Context{})
+	router.Error((*Context).ErrorHandler)
+	admin := router.Subrouter(Context{}, "/admin")
+	admin.Error((*Context).ErrorHandlerSecondary)
+	admin.Get("/foo", (*Context).ErrorAction)
+
+	rw, req := newTestRequest("GET", "/admin/foo")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "My Secondary Error", 500)
 }
