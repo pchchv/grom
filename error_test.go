235a72efac7941fe059caf188c9431ac26d90f40
pchchv <ipchchv@gmail.com>
2024-01-27 12:17:40 +0300

    multiple error handlers tests

--- error_test.go
+++ error_test.go
@@ -43,3 +43,46 @@ func TestHandlerOnRoot(t *testing.T) {
 	router.ServeHTTP(rw, req)
 	assertResponse(t, rw, "My Error", http.StatusInternalServerError)
 }
+
+func TestMultipleErrorHandlers(t *testing.T) {
+	router := New(Context{})
+	router.Error((*Context).ErrorHandler)
+	router.Get("/action", (*Context).ErrorAction)
+
+	admin := router.Subrouter(AdminContext{}, "/admin")
+	admin.Error((*AdminContext).ErrorHandler)
+	admin.Get("/action", (*AdminContext).ErrorAction)
+
+	rw, req := newTestRequest("GET", "/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "My Error", http.StatusInternalServerError)
+
+	rw, req = newTestRequest("GET", "/admin/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "Admin Error", http.StatusInternalServerError)
+}
+
+func TestMultipleErrorHandlers2(t *testing.T) {
+	router := New(Context{})
+	router.Get("/action", (*Context).ErrorAction)
+
+	admin := router.Subrouter(AdminContext{}, "/admin")
+	admin.Error((*AdminContext).ErrorHandler)
+	admin.Get("/action", (*AdminContext).ErrorAction)
+
+	api := router.Subrouter(APIContext{}, "/api")
+	api.Error((*APIContext).ErrorHandler)
+	api.Get("/action", (*APIContext).ErrorAction)
+
+	rw, req := newTestRequest("GET", "/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "Application Error", http.StatusInternalServerError)
+
+	rw, req = newTestRequest("GET", "/admin/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "Admin Error", http.StatusInternalServerError)
+
+	rw, req = newTestRequest("GET", "/api/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "Api Error", http.StatusInternalServerError)
+}
