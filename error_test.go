cf45bcf08d043ce7eca8da415aa8948394db6a6b
pchchv <ipchchv@gmail.com>
2024-01-27 12:16:41 +0300

    no error handler and on root handler tests

--- error_test.go
+++ error_test.go
@@ -3,9 +3,43 @@ package grom
 import (
 	"fmt"
 	"net/http"
+	"testing"
 )
 
 func ErrorHandlerWithNoContext(w ResponseWriter, r *Request, err interface{}) {
 	w.WriteHeader(http.StatusInternalServerError)
 	fmt.Fprintf(w, "Contextless Error")
+}
+
+func TestNoErrorHandler(t *testing.T) {
+	router := New(Context{})
+	router.Get("/action", (*Context).ErrorAction)
+
+	admin := router.Subrouter(AdminContext{}, "/admin")
+	admin.Get("/action", (*AdminContext).ErrorAction)
+
+	rw, req := newTestRequest("GET", "/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "Application Error", http.StatusInternalServerError)
+
+	rw, req = newTestRequest("GET", "/admin/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "Application Error", http.StatusInternalServerError)
+}
+
+func TestHandlerOnRoot(t *testing.T) {
+	router := New(Context{})
+	router.Error((*Context).ErrorHandler)
+	router.Get("/action", (*Context).ErrorAction)
+
+	admin := router.Subrouter(AdminContext{}, "/admin")
+	admin.Get("/action", (*AdminContext).ErrorAction)
+
+	rw, req := newTestRequest("GET", "/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "My Error", http.StatusInternalServerError)
+
+	rw, req = newTestRequest("GET", "/admin/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "My Error", http.StatusInternalServerError)
 }
