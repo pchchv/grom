8f829e49c6f4d58570eff90047ee4b49a5853872
pchchv <ipchchv@gmail.com>
2024-01-28 11:11:25 +0300

    Implement a static middleware and static middleware option prefix tests

--- static_middleware_test.go
+++ static_middleware_test.go
@@ -1,6 +1,10 @@
 package grom
 
-import "os"
+import (
+	"os"
+	"strings"
+	"testing"
+)
 
 func routerSetupBody() string {
 	fileBytes, _ := os.ReadFile(testFilename())
@@ -9,4 +13,45 @@ func routerSetupBody() string {
 
 func testFilename() string {
 	return "router_setup.go"
+}
+
+func TestStaticMiddleware(t *testing.T) {
+	currentRoot, _ := os.Getwd()
+
+	router := New(Context{})
+	router.Middleware(StaticMiddleware(currentRoot))
+	router.Get("/action", (*Context).A)
+
+	// Make sure we can still hit actions:
+	rw, req := newTestRequest("GET", "/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "context-A", 200)
+
+	rw, req = newTestRequest("GET", "/"+testFilename())
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, strings.TrimSpace(routerSetupBody()), 200)
+
+	rw, req = newTestRequest("POST", "/"+testFilename())
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "Not Found", 404)
+}
+
+func TestStaticMiddlewareOptionPrefix(t *testing.T) {
+	currentRoot, _ := os.Getwd()
+
+	router := New(Context{})
+	router.Middleware(StaticMiddleware(currentRoot, StaticOption{Prefix: "/public"}))
+	router.Get("/action", (*Context).A)
+
+	rw, req := newTestRequest("GET", "/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "context-A", 200)
+
+	rw, req = newTestRequest("GET", "/"+testFilename())
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "Not Found", 404)
+
+	rw, req = newTestRequest("GET", "/public/"+testFilename())
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, strings.TrimSpace(routerSetupBody()), 200)
 }
