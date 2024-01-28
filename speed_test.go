4f644b12008da212a8f770561ae37700af4350a0
pchchv <ipchchv@gmail.com>
2024-01-28 11:18:54 +0300

    Implement a Header, Write and WriteHeader methods

--- speed_test.go
+++ speed_test.go
@@ -1,4 +1,16 @@
 package grom
 
+import "net/http"
+
 // Null response writer
 type NullWriter struct{}
+
+func (w *NullWriter) Header() http.Header {
+	return nil
+}
+
+func (w *NullWriter) Write(data []byte) (n int, err error) {
+	return len(data), nil
+}
+
+func (w *NullWriter) WriteHeader(statusCode int) {}
