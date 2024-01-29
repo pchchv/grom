29a93ad7ed96b594bc2ea2c54724091539c356f7
pchchv <ipchchv@gmail.com>
2024-01-29 13:31:16 +0300

    Implement a bad method and with handler tests

--- not_found_test.go
+++ not_found_test.go
@@ -31,3 +31,19 @@ func TestNoHandler(t *testing.T) {
 	router.ServeHTTP(rw, req)
 	assertResponse(t, rw, "Not Found", http.StatusNotFound)
 }
+
+func TestBadMethod(t *testing.T) {
+	router := New(Context{})
+	rw, req := newTestRequest("POOP", "/this_path_doesnt_exist")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "Not Found", http.StatusNotFound)
+}
+
+func TestWithHandler(t *testing.T) {
+	router := New(Context{})
+	router.NotFound(MyNotFoundHandler)
+
+	rw, req := newTestRequest("GET", "/this_path_doesnt_exist")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "My Not Found", http.StatusNotFound)
+}
