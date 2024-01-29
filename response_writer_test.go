98005bd42fc84011324fe0ee1a359e4a13eeb864
pchchv <ipchchv@gmail.com>
2024-01-29 13:16:33 +0300

    Implement a Header, Write and WriteHeader methods

--- response_writer_test.go
+++ response_writer_test.go
@@ -1,5 +1,17 @@
 package grom
 
+import "net/http"
+
 type hijackableResponse struct {
 	Hijacked bool
+}
+
+func (h *hijackableResponse) Header() http.Header {
+	return nil
+}
+func (h *hijackableResponse) Write(buf []byte) (int, error) {
+	return 0, nil
+}
+func (h *hijackableResponse) WriteHeader(code int) {
+	// no-op
 }
