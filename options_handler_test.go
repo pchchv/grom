1b2f9da01c8228eefdf6c14970086b5e07bfe30d
pchchv <ipchchv@gmail.com>
2024-01-29 13:26:46 +0300

    Implement a options handler and custom options handler tests

--- options_handler_test.go
+++ options_handler_test.go
@@ -1,6 +1,11 @@
 package grom
 
-import "strings"
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func (c *Context) OptionsHandler(rw ResponseWriter, req *Request, methods []string) {
 	rw.Header().Add("Access-Control-Allow-Methods", strings.Join(methods, ", "))
@@ -10,4 +15,39 @@ func (c *Context) OptionsHandler(rw ResponseWriter, req *Request, methods []stri
 func AccessControlMiddleware(rw ResponseWriter, req *Request, next NextMiddlewareFunc) {
 	rw.Header().Add("Access-Control-Allow-Origin", "*")
 	next(rw, req)
+}
+
+func TestOptionsHandler(t *testing.T) {
+	router := New(Context{})
+
+	sub := router.Subrouter(Context{}, "/sub")
+	sub.Middleware(AccessControlMiddleware)
+	sub.Get("/action", (*Context).A)
+	sub.Put("/action", (*Context).A)
+
+	rw, req := newTestRequest("OPTIONS", "/sub/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "", 200)
+	assert.Equal(t, "GET, PUT", rw.Header().Get("Access-Control-Allow-Methods"))
+	assert.Equal(t, "*", rw.Header().Get("Access-Control-Allow-Origin"))
+
+	rw, req = newTestRequest("GET", "/sub/action")
+	router.ServeHTTP(rw, req)
+	assert.Equal(t, "*", rw.Header().Get("Access-Control-Allow-Origin"))
+}
+
+func TestCustomOptionsHandler(t *testing.T) {
+	router := New(Context{})
+	router.OptionsHandler((*Context).OptionsHandler)
+
+	sub := router.Subrouter(Context{}, "/sub")
+	sub.Middleware(AccessControlMiddleware)
+	sub.Get("/action", (*Context).A)
+	sub.Put("/action", (*Context).A)
+
+	rw, req := newTestRequest("OPTIONS", "/sub/action")
+	router.ServeHTTP(rw, req)
+	assertResponse(t, rw, "", 200)
+	assert.Equal(t, "GET, PUT", rw.Header().Get("Access-Control-Allow-Methods"))
+	assert.Equal(t, "100", rw.Header().Get("Access-Control-Max-Age"))
 }
