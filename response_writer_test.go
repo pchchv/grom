c1e4169cc70b21dc2ceb5ddbd9ae596bd9a471fe
pchchv <ipchchv@gmail.com>
2024-01-29 13:17:46 +0300

    Implement a Flush, Hijack and CloseNotify methods

--- response_writer_test.go
+++ response_writer_test.go
@@ -1,6 +1,10 @@
 package grom
 
-import "net/http"
+import (
+	"bufio"
+	"net"
+	"net/http"
+)
 
 type hijackableResponse struct {
 	Hijacked bool
@@ -9,9 +13,24 @@ type hijackableResponse struct {
 func (h *hijackableResponse) Header() http.Header {
 	return nil
 }
+
 func (h *hijackableResponse) Write(buf []byte) (int, error) {
 	return 0, nil
 }
+
 func (h *hijackableResponse) WriteHeader(code int) {
 	// no-op
+}
+
+func (h *hijackableResponse) Flush() {
+	// no-op
+}
+
+func (h *hijackableResponse) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	h.Hijacked = true
+	return nil, nil, nil
+}
+
+func (h *hijackableResponse) CloseNotify() <-chan bool {
+	return nil
 }
