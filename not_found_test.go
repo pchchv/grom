81e336846cd372470a1acb25b8edf0b27948001f
pchchv <ipchchv@gmail.com>
2024-01-29 13:28:45 +0300

    Implement a HandlerWithContext method

---
+++ not_found_test.go
@@ -0,0 +1,11 @@
+package grom
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (c *Context) HandlerWithContext(rw ResponseWriter, r *Request) {
+	rw.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(rw, "My Not Found With Context")
+}
