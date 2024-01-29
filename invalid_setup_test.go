0fb2da0a8799bfb8de102f6e23e662d9175049d9
pchchv <ipchchv@gmail.com>
2024-01-29 13:34:29 +0300

    Add an invalidSubcontext type and its method Handler

--- invalid_setup_test.go
+++ invalid_setup_test.go
@@ -7,3 +7,7 @@ func (c *Context) InvalidHandler2(w ResponseWriter, r *Request) string {
 }
 
 func (c *Context) InvalidHandler3(w ResponseWriter, r ResponseWriter) {}
+
+type invalidSubcontext struct{}
+
+func (c *invalidSubcontext) Handler(w ResponseWriter, r *Request) {}
