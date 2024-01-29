f780d32931023f444c23ff89658c73a94b23dcf3
pchchv <ipchchv@gmail.com>
2024-01-29 13:25:56 +0300

    Implement an AccessControlMiddleware func

--- options_handler_test.go
+++ options_handler_test.go
@@ -6,3 +6,8 @@ func (c *Context) OptionsHandler(rw ResponseWriter, req *Request, methods []stri
 	rw.Header().Add("Access-Control-Allow-Methods", strings.Join(methods, ", "))
 	rw.Header().Add("Access-Control-Max-Age", "100")
 }
+
+func AccessControlMiddleware(rw ResponseWriter, req *Request, next NextMiddlewareFunc) {
+	rw.Header().Add("Access-Control-Allow-Origin", "*")
+	next(rw, req)
+}
