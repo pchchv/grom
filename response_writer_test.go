d493f1b64258f74dd4e233c9711dae5376ee5178
pchchv <ipchchv@gmail.com>
2024-01-29 13:20:33 +0300

    Implement a response writer write and response writer write header tests

--- response_writer_test.go
+++ response_writer_test.go
@@ -5,6 +5,9 @@ import (
 	"net"
 	"net/http"
 	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type hijackableResponse struct {
@@ -47,4 +50,31 @@ func (c *closeNotifyingRecorder) CloseNotify() <-chan bool {
 
 func (c *closeNotifyingRecorder) close() {
 	c.closed <- true
+}
+
+func TestResponseWriterWrite(t *testing.T) {
+	rec := httptest.NewRecorder()
+	rw := ResponseWriter(&appResponseWriter{ResponseWriter: rec})
+
+	assert.Equal(t, rw.Written(), false)
+
+	n, err := rw.Write([]byte("Hello world"))
+	assert.Equal(t, n, 11)
+	assert.NoError(t, err)
+
+	assert.Equal(t, n, 11)
+	assert.Equal(t, rec.Code, rw.StatusCode())
+	assert.Equal(t, rec.Code, http.StatusOK)
+	assert.Equal(t, rec.Body.String(), "Hello world")
+	assert.Equal(t, rw.Size(), 11)
+	assert.Equal(t, rw.Written(), true)
+}
+
+func TestResponseWriterWriteHeader(t *testing.T) {
+	rec := httptest.NewRecorder()
+	rw := ResponseWriter(&appResponseWriter{ResponseWriter: rec})
+
+	rw.WriteHeader(http.StatusNotFound)
+	assert.Equal(t, rec.Code, rw.StatusCode())
+	assert.Equal(t, rec.Code, http.StatusNotFound)
 }
